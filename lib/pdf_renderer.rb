dec720b02409ab6ef94805b4a1662c19b00d4ff1
residenti <n.queue.r@gmail.com>
2021-06-06 17:59:44 +0900

    wrote pdf_renderer

--- lib/pdf_renderer.rb
+++ lib/pdf_renderer.rb
@@ -1,6 +1,7 @@
-require "pdf_renderer/version"
-require "pdf_renderer/railtie"
+require "prawn"
 
-module PdfRenderer
-  # Your code goes here...
+ActionController::Renderers.add :pdf do |filename, options|
+  pdf = Prawn::Document.new
+  pdf.text render_to_string(options)
+  send_data(pdf.render, filename: "#{filename}.pdf", disposition: "attachment")
 end
